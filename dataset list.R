f35e7c609a108456ef01f03845ebc316174264a2
mfglabs <mfglabs@MacBook-Pro-6.local>
2018-02-07 17:26:52 +0100

    list of links towards available data

---
+++ dataset list.R
@@ -0,0 +1,51 @@
+
+# List of all data sources
+
+# Cycling:
+
+# A symbolic line representation of the cycling paths from LTA & Town Council.
+# https://data.gov.sg/dataset/cycling-path-network
+
+# Data display the location, counts and type of bicycle parking at RTS Stations
+# https://data.gov.sg/dataset/lta-bicycle-rack
+
+# Public transport:
+
+# Average daily number of trips made islandwide on MRT, LRT, bus & taxi.
+# https://data.gov.sg/dataset/public-transport-utilisation-average-public-transport-ridership
+
+# Average trip distance made islandwide on MRT, LRT, bus & taxi.
+# https://data.gov.sg/dataset/public-transport-utilisation-average-trip-distance
+
+# Passenger journeys based on public transport mode (MRT, LRT and public buses).
+# https://data.gov.sg/dataset/public-transport-journeys-average-distance-per-trip
+
+# Indicative line showing the MRT, LRT and Railway network.
+# https://data.gov.sg/dataset/master-plan-2014-rail-line
+
+# Airport:
+
+# Air Passenger Arrivals by Region and Selected Country of Embarkation.
+# https://data.gov.sg/dataset/air-passenger-arrivals-total-by-region-and-selected-country-of-embarkation?resource_id=efd7b75b-992e-4221-aff3-c57140af1f67
+
+# Taxi:
+
+# Monthly Taxi Population by Company
+# https://data.gov.sg/dataset/monthly-taxi-population-by-company
+
+# Carparks and POI
+
+# Lists parking rates & opening hours for carparks in major shopping malls, attractions, and hotels.
+# https://data.gov.sg/dataset/carpark-rates
+
+
+# Demographics:
+# Number of people by zone
+# http://www.singstat.gov.sg/statistics/browse-by-theme/geographic-distribution
+
+# income by area
+# https://www.singstat.gov.sg/docs/default-source/default-document-library/publications/publications_and_papers/cop2010/census_2010_release3/excel/t18-22.xls
+
+# Travel time
+# citymapper api: https://citymapper.com/tools/905/travel-time
+# osm
